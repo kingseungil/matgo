0a3ce2ac0316e705f2060d0afe7e1f52ff291106
Kingseungil <flykim511@naver.com>
2023-12-13 21:01:00 +0900

    Initial commit

---
+++ src/main/java/matgo/MatgoApplication.java
@@ -0,0 +1,13 @@
+package com.project.zbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matgo/MatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
