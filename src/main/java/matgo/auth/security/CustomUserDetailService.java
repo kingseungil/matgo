920a87ba19c58a6f5d062ffaa2048fcd4f06ead6
Kingseungil <flykim511@naver.com>
2023-12-20 23:38:17 +0900

    feat : user,admin 권한설정 (#9)

---
+++ src/main/java/matgo/auth/security/CustomUserDetailService.java
@@ -0,0 +1,46 @@
+package matgo.auth.security;
+
+import static matgo.global.exception.ErrorCode.NOT_ACTIVATED_USER;
+import static matgo.global.exception.ErrorCode.NOT_FOUND_MEMBER;
+import static matgo.member.domain.type.UserRole.ROLE_USER;
+
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import matgo.auth.exception.AuthException;
+import matgo.member.domain.entity.Member;
+import matgo.member.domain.repository.MemberRepository;
+import matgo.member.exception.MemberException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                               .map(this::createUser)
+                               .orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
+    }
+
+    private User createUser(Member member) {
+        if (!member.isVerified()) {
+            throw new AuthException(NOT_ACTIVATED_USER);
+        }
+
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(ROLE_USER.name());
+
+        return new User(String.valueOf(member.getId()),
+          member.getPassword(),
+          Collections.singleton(authority));
+    }
+}
