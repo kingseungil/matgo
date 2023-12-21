ed5820940ca7f958480e83df3c3747b1e9167246
Kingseungil <flykim511@naver.com>
2023-12-21 20:35:30 +0900

    refactor: 피드백 반영 (#16)
    - AdminDetailsService 삭제
    - 로그인 시 권한체크 추가

--- src/main/java/matgo/auth/application/AuthService.java
+++ src/main/java/matgo/auth/application/AuthService.java
@@ -1,5 +1,6 @@
 package matgo.auth.application;
 
+import static matgo.global.exception.ErrorCode.UNAUTHORIZED;
 import static matgo.global.exception.ErrorCode.WRONG_PASSWORD;
 
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,10 @@ import matgo.auth.dto.request.LoginRequest;
 import matgo.auth.dto.response.LoginResponse;
 import matgo.auth.exception.AuthException;
 import matgo.auth.jwt.JwtTokenProvider;
-import matgo.auth.security.UserDetailServiceSelector;
+import matgo.auth.security.CustomUserDetailService;
 import matgo.global.util.SecurityUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +24,13 @@ public class AuthService {
     private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailServiceSelector userDetailServiceSelector;
+    private final CustomUserDetailService customUserDetailService;
     private final TokenService tokenService;
 
 
     public LoginResponse login(LoginRequest request) {
         UserDetails userDetails = getUserDetails(request);
+        checkRole(request.role().name(), userDetails.getAuthorities().iterator().next().getAuthority());
         checkPassword(request.password(), userDetails.getPassword());
         Authentication authentication = SecurityUtil.authenticate(userDetails);
 
@@ -42,10 +43,14 @@ public class AuthService {
     }
 
     private UserDetails getUserDetails(LoginRequest request) {
-        UserDetailsService userDetailsService = userDetailServiceSelector.select(request.role());
-        return userDetailsService.loadUserByUsername(request.email());
+        return customUserDetailService.loadUserByUsername(request.email());
     }
 
+    private void checkRole(String requestRole, String actualRole) {
+        if (!requestRole.equals(actualRole)) {
+            throw new AuthException(UNAUTHORIZED);
+        }
+    }
 
     private void checkPassword(String password, String encodedPassword) {
         if (!passwordEncoder.matches(password, encodedPassword)) {
