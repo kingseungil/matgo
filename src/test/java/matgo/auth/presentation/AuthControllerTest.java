9ca5b369c317b020662f28c136a72127869b7cca
Kingseungil <flykim511@naver.com>
2023-12-24 16:55:51 +0900

    test: 비밀번호 재설정 (#10)

--- src/test/java/matgo/auth/presentation/AuthControllerTest.java
+++ src/test/java/matgo/auth/presentation/AuthControllerTest.java
@@ -2,6 +2,7 @@ package matgo.auth.presentation;
 
 import static matgo.auth.presentation.AuthDocument.loginDocument;
 import static matgo.auth.presentation.AuthDocument.logoutDocument;
+import static matgo.auth.presentation.AuthDocument.sendTemporayPasswordDocument;
 import static matgo.auth.presentation.AuthDocument.verifyEmailCodeDocument;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -12,14 +13,14 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import matgo.auth.dto.request.EmailVerificationRequest;
+import matgo.auth.dto.request.LoginRequest;
 import matgo.common.BaseControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AuthControllerTest extends BaseControllerTest {
 
-
-    public static ExtractableResponse<Response> loginMember() {
+    public static ExtractableResponse<Response> loginMember(LoginRequest loginRequest) {
         return customGiven()
           .contentType(ContentType.JSON)
           .body(loginRequest)
@@ -43,7 +44,7 @@ public class AuthControllerTest extends BaseControllerTest {
           .post("/api/auth/verify-emailcode");
 
         // then
-        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.statusCode()).isEqualTo(204);
     }
 
     @Test
@@ -71,6 +72,19 @@ public class AuthControllerTest extends BaseControllerTest {
           .delete("/api/auth/logout");
 
         // then
-        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.statusCode()).isEqualTo(204);
+    }
+
+    @Test
+    @DisplayName("임시 비밀번호 발급")
+    void sendTemporaryPassword() {
+        // given,when
+        Response response = customGivenWithDocs(sendTemporayPasswordDocument())
+          .contentType(ContentType.JSON)
+          .body(sendTemporaryPasswordRequest)
+          .post("/api/auth/send-temporary-password");
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(204);
     }
 }
