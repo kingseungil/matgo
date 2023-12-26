cac2e34cd571ceed896e3d2d6600869ca8a5a33a
Kingseungil <flykim511@naver.com>
2023-12-26 14:21:15 +0900

    feat: openAPI 데이터 DB에 저장 (#4)
    - 한달에 한번씩 스케쥴링

--- src/main/java/matgo/restaurant/feignclient/dto/RestaurantData.java
+++ src/main/java/matgo/restaurant/feignclient/dto/RestaurantData.java
@@ -6,8 +6,8 @@ public record RestaurantData(
   @JsonProperty("식당명") String name,
   @JsonProperty("도로명주소") String address,
   @JsonProperty("식당대표전화번호") String phoneNumber,
-  @JsonProperty("식당위도") String lat,
-  @JsonProperty("식당경도") String lon,
+  @JsonProperty("식당위도") Double lat,
+  @JsonProperty("식당경도") Double lon,
   @JsonProperty("음식점소개내용") String description
 ) {
 
