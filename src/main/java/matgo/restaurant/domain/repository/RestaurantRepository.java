63ca0dc6ee7d890e0d59bff67d2737b9aa6e5865
Kingseungil <flykim511@naver.com>
2023-12-26 21:02:45 +0900

    feat: 식당 조회 api (#4)
    - 전체 식당 조회
    - 주소로 식당 조회

--- src/main/java/matgo/restaurant/domain/repository/RestaurantRepository.java
+++ src/main/java/matgo/restaurant/domain/repository/RestaurantRepository.java
@@ -4,10 +4,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import matgo.restaurant.domain.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findByModifiedAtAfter(LocalDateTime time);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.name IN :names AND r.address IN :addresses")
+    List<Restaurant> findByNameInAndAddressIn(List<String> names, List<String> addresses);
 }
