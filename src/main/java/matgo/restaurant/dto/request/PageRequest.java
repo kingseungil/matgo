a1bb49467768452c1bd8c376841c0734c253a8b7
Kingseungil <flykim511@naver.com>
2023-12-27 22:27:44 +0900

    feat: 각 조회 시 별점 순, 리뷰 개수 순으로 정렬기능 추가 (#5)

---
+++ src/main/java/matgo/restaurant/dto/request/PageRequest.java
@@ -0,0 +1,24 @@
+package matgo.restaurant.dto.request;
+
+import jakarta.validation.constraints.Min;
+import java.util.Optional;
+import org.springframework.data.domain.Sort;
+
+public record PageRequest(
+
+  @Min(0)
+  int page,
+  @Min(1)
+  int size,
+  Optional<Sort.Direction> direction,
+  Optional<String> sortBy
+) {
+
+    public Sort getSort() {
+        if (direction.isEmpty() || sortBy.isEmpty()) {
+            return Sort.unsorted();
+        } else {
+            return Sort.by(direction.get(), sortBy.get());
+        }
+    }
+}
