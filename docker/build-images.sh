c35651fc6107a468209f34528ffa84fb7ba844e0
Kingseungil <flykim511@naver.com>
2024-01-08 18:20:27 +0900

    feat: docker-compose
    - nginx 추가
    - docker image push

---
+++ docker/build-images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ -f .env ]]; then
+  export $(cat .env | grep -v '^#' | xargs)
+fi
+
+VERSION="1.0.0"
+HUB_USER="$MY_HUB_ID"
+
+cd ..
+
+echo "Building image : api"
+./gradlew clean build -x test
+#./gradlew build
+echo "Dockerfile을 이용하여 이미지 생성"
+docker build -f docker/api/Dockerfile -t "matgo-api:$VERSION" .
+echo "생성된 이미지를 docker hub에 업로드"
+docker tag "matgo-api:$VERSION" "$HUB_USER/matgo-api:$VERSION"
+echo "docker push"
+docker push "$HUB_USER/matgo-api:$VERSION"
+
+echo "image built successfully."
