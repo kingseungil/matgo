a3deaab822edd53f452004dc7b5515870f879249
Kingseungil <flykim511@naver.com>
2024-01-08 18:20:27 +0900

    feat: docker-compose
    - nginx 추가
    - docker image push

--- src/main/java/matgo/global/config/ElasticSearchConfig.java
+++ src/main/java/matgo/global/config/ElasticSearchConfig.java
@@ -1,5 +1,6 @@
 package matgo.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -11,10 +12,16 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchAuditing
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                                  .connectedTo("localhost:9200")
+                                  .connectedTo(host + ":" + port)
                                   .build();
     }
 }
